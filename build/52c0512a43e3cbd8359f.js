0ccf1bd26ec76d6603eef3ebcb4eeaefdd6894f9
Radon4ik <Loboda.Radimir@gmail.com>
2022-11-22 21:17:04 +0200

    commit

---
+++ build/52c0512a43e3cbd8359f.js
@@ -0,0 +1,1 @@
+import React from"react";import{createRoot}from"react-dom/client";import"./index.scss";import Application from"./Application";createRoot(document.querySelector(".root")).render(React.createElement(Application,null));
--- webpack.config.js
+++ webpack.config.js
@@ -2,75 +2,64 @@ const { resolve, join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (_, argv) => {
-  const isDevelopment = argv.mode === 'development';
-
-  return {
-    entry: './src/index.js',
-    output: {
-      path: resolve(__dirname, 'dist'),
-      filename: '[name]-[fullhash].js',
-      clean: true,
-      publicPath: '/',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: '[name]-[fullhash].js',
+    clean: true,
+  },
+  performance: {
+    hints: false,
+    maxAssetSize: 512000,
+    maxEntrypointSize: 51200,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           },
         },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'css-loader',
-            'postcss-loader',
-            'sass-loader',
-          ],
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg)$/i,
-          type: 'asset/resource', // use /inline if dont work
-          parser: {
-            dataUrlCondition: {
-              limit: 30 * 1024,
-            },
-          },
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'fonts/[hash][ext][query]',
-          },
-        },
-        {
-          test: /\.html$/i,
-          loader: 'html-loader',
-        },
-      ],
-    },
-    devServer: {
-      hot: true,
-      port: 3000,
-      static: {
-        directory: join(__dirname, 'src'),
       },
-      historyApiFallback: true,
-    },
-    devtool: isDevelopment
-      ? 'eval-cheap-module-source-map'
-      : 'hidden-nosources-source-map',
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-      new MiniCssExtractPlugin({
-        filename: '[name].[fullhash].css',
-      }),
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(html)$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
-  };
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name]-[fullhash].css',
+    }),
+  ],
+
+  devServer: {
+    port: 8080,
+    compress: true,
+    hot: true,
+    static: {
+      directory: join(__dirname, 'src'),
+    },
+  },
+
+  devtool: 'inline-source-map',
 };
